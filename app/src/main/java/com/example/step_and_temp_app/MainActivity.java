84f8ba8ed2523fd9e3e2e69514bd42c2be2dc31b
aightImmaProgram <mobn21@student.aau.dk>
2022-04-17 11:13:22 +0200

    Ambient temperature IMPLEMENTED
    Ambient temperature er blevet implemented og kan bruges, men er ikke blevet brugt i noget logik endnu.

--- app/src/main/java/com/example/step_and_temp_app/MainActivity.java
+++ app/src/main/java/com/example/step_and_temp_app/MainActivity.java
@@ -3,15 +3,22 @@ package com.example.step_and_temp_app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+
     private TextView stepCounterText;
+
     private SensorManager sensorManager;
+
     private Sensor stepSensor;
+    private Sensor ambientTemp;
     private boolean isSensorPresent;
+
 
 
     @Override
@@ -20,5 +27,37 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         sensorManager =  (SensorManager) getSystemService(SENSOR_SERVICE);
+        ambientTemp = sensorManager.getDefaultSensor(Sensor.TYPE_AMBIENT_TEMPERATURE);
+
+        if (sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER)!=null)
+        {
+            stepSensor = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
+            isSensorPresent
+        }
+        else
+        {
+
+        }
+    }
+
+    protected void onResume() {        //Since this method has the protected access modifier, it can only be accessed within its own package,
+                                       //and by a subclass of its class in another package
+        super.onResume();
+        sensorManager.registerListener(this, ambientTemp, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    protected void onPause() {
+        super.onPause();
+        sensorManager.unregisterListener(this);
+    }
+
+    @Override   //IS NOT USED but methods need to be implemented since SensorEventListener is implemented by MainActivity
+    public void onSensorChanged(SensorEvent sensorEvent) {
+
+    }
+
+    @Override   //IS NOT USED but methods need to be implemented since SensorEventListener is implemented by MainActivity
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
     }
 }
