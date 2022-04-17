dcb9bbc58ac0321d66290b29fffa6b5c604ea92d
marchusl <marchusl@gmail.com>
2022-04-17 11:06:35 +0200

    yo

--- app/src/main/java/com/example/step_and_temp_app/MainActivity.java
+++ app/src/main/java/com/example/step_and_temp_app/MainActivity.java
@@ -3,11 +3,13 @@ package com.example.step_and_temp_app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
     private TextView stepCounterText;
     private SensorManager sensorManager;
     private Sensor stepSensor;
@@ -19,16 +21,31 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        stepCounterText = findViewById(R.id.stepCounterText);
+
         sensorManager =  (SensorManager) getSystemService(SENSOR_SERVICE);
 
         if (sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER)!=null)
         {
             stepSensor = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
-            isSensorPresent
+            isSensorPresent = true;
         }
         else
         {
-
+            stepCounterText.setText("No step counter detected");
+            isSensorPresent = false;
         }
     }
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
+    }
+
+    
 }
